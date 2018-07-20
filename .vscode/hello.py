4c21333386444c2df04987b3c925ded4f57e537d
GVK <gvk@gvk.org>
2018-07-20 19:22:45 +0530

    test merge conflicts

--- .vscode/hello.py
+++ .vscode/hello.py
@@ -4,16 +4,19 @@ class car:
     year=""
     brand=""
     noOfOwners=0
-    def __init__(self, color, model,year,brand,noOfOwners):
+    regNo=""
+    def __init__(self, color, model,year,brand,noOfOwners,regNo):
         self.color=color
         self.model=model
         self.year=year
         self.brand=brand
         self.noOfOwners=noOfOwners
-    def displayMyCar(self):
-        print("\n",self.color,"\n",self.model,"\n",self.year,"\n",self.brand,"\n",self.noOfOwners)
+        self.regNo=regNo
 
-MyCar=car("Red","Sedan","2018","Rolls Royace","10")
+    def displayMyCar(self):
+        print("\n",self.color,"\n",self.model,"\n",self.year,"\n",self.brand,"\n",self.noOfOwners,"\n",self.regNo)
+
+MyCar=car("Red","Sedan","2018","BMW","10","TS07")
 MyCar.displayMyCar()
 
     
