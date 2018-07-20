f418d10ae273280bb2075b6c2b12f6cc3b10acae
GVK <gvk@gvk.org>
2018-07-20 16:32:16 +0530

    changed objectclass file and hello

--- .vscode/ClassAndObjectExample.py
+++ .vscode/ClassAndObjectExample.py
@@ -6,7 +6,12 @@ class Person:
     print("Hello my name is \n" + self.name)
     print("Hello my age is \n" + self.age)
 
-name=input("please enter your name for creating candidate  \n")
+name=input("please enter your name for creating candidate:-\n")
 age=input("please enter your candidate age\n")
 p1 = Person(name, age)
 p1.myfunc()
+del p1
+try:
+  print(p1)
+except NameError:
+  print("object is deleted")
