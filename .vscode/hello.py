e80bfde430e9cb8a43e04e39d844086d39e7b4f2
GVK <gvk@gvk.org>
2018-07-20 16:35:37 +0530

    hello file added

--- .vscode/hello.py
+++ .vscode/hello.py
@@ -1,10 +1,19 @@
-class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+class car:
+    color=""
+    model=""
+    year=""
+    brand=""
+    noOfOwners=0
+    def __init__(self, color, model,year,brand,noOfOwners):
+        self.color=color
+        self.model=model
+        self.year=year
+        self.brand=brand
+        self.noOfOwners=noOfOwners
+    def displayMyCar(self):
+        print("\n",self.color,"\n",self.model,"\n",self.year,"\n",self.brand,"\n",self.noOfOwners)
 
-  def myfunc(self,abc):
-    print("Hello my name is " + abc.name)
+MyCar=car("Red","Sedan","2018","Rolls Royace","10")
+MyCar.displayMyCar()
 
-p1 = Person("uday",25)
-p1.myfunc(p1)
+    
