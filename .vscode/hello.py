0152a20c2499e420fc7c86b45c527a51ffbfb559
GVK <gvk@gvk.org>
2018-07-20 19:52:50 +0530

    Testing for pull request

--- .vscode/hello.py
+++ .vscode/hello.py
@@ -5,18 +5,20 @@ class car:
     brand=""
     noOfOwners=0
     regNo=""
-    def __init__(self, color, model,year,brand,noOfOwners,regNo):
+    area=""
+    def __init__(self, color, model,year,brand,noOfOwners,regNo,area):
         self.color=color
         self.model=model
         self.year=year
         self.brand=brand
         self.noOfOwners=noOfOwners
         self.regNo=regNo
+        self.area=area
 
     def displayMyCar(self):
-        print("\n",self.color,"\n",self.model,"\n",self.year,"\n",self.brand,"\n",self.noOfOwners,"\n",self.regNo)
+        print("\n",self.color,"\n",self.model,"\n",self.year,"\n",self.brand,"\n",self.noOfOwners,"\n",self.regNo,"\n",self.area)
 
-MyCar=car("Red","Sedan","2018","BMW","10","TS07")
+MyCar=car("Red","Sedan","2018","BMW","10","TS07","miyapur")
 MyCar.displayMyCar()
 
     
